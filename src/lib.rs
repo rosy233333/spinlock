75270f7d2e2f0e003491693deebfb07e4257bf79
Youjie Zheng <Azure_stars@126.com>
2024-06-06 09:51:52 +0800

    feat: add IrqOnly lock

--- src/lib.rs
+++ src/lib.rs
@@ -12,9 +12,16 @@
 
 mod base;
 
-use kernel_guard::{NoOp, NoPreempt, NoPreemptIrqSave};
+use kernel_guard::{IrqSave, NoOp, NoPreempt, NoPreemptIrqSave};
 
 pub use self::base::{BaseSpinLock, BaseSpinLockGuard};
+
+/// A spin lock that disables local IRQs while trying to lock, and
+/// re-enables it after unlocking.
+pub type SpinNoOnlyIrq<T> = BaseSpinLock<IrqSave, T>;
+
+/// A guard that provides mutable data access for [`SpinNoOnlyIrq`].
+pub type SpinNoOnlyIrqGuard<'a, T> = BaseSpinLockGuard<'a, IrqSave, T>;
 
 /// A spin lock that disables kernel preemption while trying to lock, and
 /// re-enables it after unlocking.
